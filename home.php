0e96d8ffefab129cc18913732f76408605389210
Aris-Georgian ILIE <arisgeorgian2004@gmail.com>
2025-05-17 12:05:41 +0300

    home.php

--- home.php
+++ home.php
@@ -142,7 +142,7 @@ template_header('Home')
 </div>
 
 <div class="recentlyadded content-wrapper">
-    <h2>Recently Added Products</h2>
+    <h2>Recently added products</h2>
     <div class="products">
         <?php foreach ($recently_added_products as $product): ?>
         <a href="index.php?page=product&id=<?=$product['id']?>" class="product">
