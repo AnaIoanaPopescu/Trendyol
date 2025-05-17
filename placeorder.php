b95d65a5be6d52c2c128edc3c285449f74733442
Rotaru Diana Maria <rotarudianardm@gmail.com>
2025-05-17 08:26:19 +0000

    Add files via upload

---
+++ placeorder.php
@@ -0,0 +1,25 @@
+
+<?=template_header('Place Order')?>
+
+    <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
+
+<style>
+h1 {
+    font-family: 'Satisfy', cursive;
+    font-size: 25px;
+    font-weight: 400;
+    color: #394352;
+    text-align: center;
+    margin-bottom: 20px;
+    letter-spacing: 1px;
+}
+</style>
+<div class="placeorder content-wrapper">
+    <h1>Your order has been placed!</h1>
+    <p>Thank you for ordering from us! We will contact you via email with your order details!</p>
+    <p>A confirmation email has been sent to your email address.</p>
+
+    <img src="imgs/anim.gif" alt="Order Confirmed" style="width: 150px; margin: 20px auto; display: block;">
+</div>
+
+<?=template_footer()?>
